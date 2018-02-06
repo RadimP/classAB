41d95ac2598e7a6cba0471e7b301a6ea64a2f896
Student <Student@192.168.0.17>
2018-02-06 20:30:16 +0100

    LM tu byl.

--- JavaApplication33/src/javaapplication33/JavaApplication33.java
+++ JavaApplication33/src/javaapplication33/JavaApplication33.java
@@ -17,7 +17,7 @@ public class JavaApplication33 {
     public static void main(String[] args) {
         B b=new B();
     
-        System.out.println(b);
+        System.out.println(b);//LM tu byl
     }
     
 }
